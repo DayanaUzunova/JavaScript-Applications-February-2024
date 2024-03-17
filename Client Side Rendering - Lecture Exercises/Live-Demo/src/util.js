7fb812c288c892a71a1130f94401d3d78817e9a3
Dayana Uzunova <daqnauzunova@abv.bg>
2024-03-17 21:58:38 +0200

    Client Side Rendering Lecture Exercises.
    Client Side Rendering Lecture Exercises.

---
+++ Client Side Rendering - Lecture Exercises/Live-Demo/src/util.js
@@ -0,0 +1,19 @@
+export function getUserData(){
+    return JSON.parse(localStorage.getItem('user'));
+}
+export function saveUserData(data){
+    localStorage.setItem('user', JSON.stringify(data));
+}
+export function clearUserData(){
+    localStorage.removeItem('user');
+}
+export function createSubmitHandler(callback){
+    return function (event){
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const entries = [...formData.entries()].map(([k, v]) => [k, v.trim()]);
+
+        callback(Object.fromEntries(entries));
+    }
+}
