fe5d0722f8ef3a3517b7e35d5b99af68ff08f05c
Dayana Uzunova <daqnauzunova@abv.bg>
2024-03-27 22:22:55 +0200

    Exam Preparation Exercises.
    Exam Preparation Exercises.

---
+++ Exam Preparation - 2/Samurider/src/util.js
@@ -0,0 +1,42 @@
+export function setUserData(data){
+    localStorage.setItem('user', JSON.stringify(data));
+}
+
+export function getUserData(){
+    return JSON.parse(localStorage.getItem('user'));
+}
+
+export function clearUserData(){
+    localStorage.removeItem('user');
+}
+
+export function isOwner(itemOwnerId){
+    return getUserData()?._id == itemOwnerId;
+}
+
+//TODO add custom validation
+export function createSubmitHandler(callback){
+    return function (event){
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = [...formData.entries()].map(([k, v]) => [k, v.trim()]);
+
+        callback(Object.fromEntries(data), event.target);
+    };
+}
+
+export function updateNav(){
+    const userNav = document.querySelector("nav .user");
+    const guestNav = document.querySelector("nav .guest");
+
+    const userData = getUserData();
+    if(userData){
+        guestNav.style.display = "none";
+        userNav.style.display = "block";
+    }
+    else {
+        guestNav.style.display = "block";
+        userNav.style.display = "none";
+    }
+}
